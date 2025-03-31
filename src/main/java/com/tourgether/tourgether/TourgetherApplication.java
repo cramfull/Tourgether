3ac79959dd3e7763e8120d39da6a3a4f1a3ad2b0
cramfull <tmdgud99@gmail.com>
2025-03-31 22:17:37 +0900

    [init]

---
+++ src/main/java/com/tourgether/tourgether/TourgetherApplication.java
@@ -0,0 +1,13 @@
+package com.tourgether.tourgether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourgetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourgetherApplication.class, args);
+	}
+
+}
