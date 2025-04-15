a119a106efa72e17c1e66ef60023fb442e0f7995
cramfull <130737411+cramfull@users.noreply.github.com>
2025-04-15 20:25:34 +0900

    fix: security config 오류 수정 (#51)
    * feat: OAuthUserInfo, auth 진행중
    
    * chore: remove .idea from tracking
    
    * pulled develop
    
    * WIP: local application.yml
    
    * feat: CustomOAuth2User, auth 진행중
    
    * feat: OAuth2Authentication Handler, auth 진행중
    
    * feat: JwtAuthenticationFilter 진행중
    
    * fix: develop 충돌 해결
    
    * feat: reissue 구현
    
    * fix: MemberService 수정, GlobalExceptionHandler 수정
    
    * fix: AuthService 컴포넌트 수정, .yml jwt-secretKey 환경 변수로 사용
    
    * fix: RefreshTokenRequest validation 추가, JwtUtil @Value 수정
    
    * feat: AccessToken, RefreshToken 발급
    
    * feat: oauth2 로그인 기능 구현
    
    * fix: AuthController @Valid 제거
    
    * fix: dto @JsonProperty 추가 후 카멜케이스로 변경
    
    * fix: dto @JsonProperty 추가 후 카멜케이스로 변경 후 strategy 수정
    
    * fix: Strategy패턴에 OAuth2ClientUtil 적용

--- src/main/java/com/tourgether/tourgether/auth/oauth/dto/KakaoUserResponse.java
+++ src/main/java/com/tourgether/tourgether/auth/oauth/dto/KakaoUserResponse.java
@@ -1,14 +1,17 @@
 package com.tourgether.tourgether.auth.oauth.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.tourgether.tourgether.member.enums.Provider;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record KakaoUserResponse(
         @NotNull
         Long id,
         @NotNull
+        @JsonProperty("kakao_account")
         KakaoAccount kakaoAccount
 ) implements OAuth2UserResponse {
     public record KakaoAccount(
--- src/main/java/com/tourgether/tourgether/common/config/SecurityConfig.java
+++ src/main/java/com/tourgether/tourgether/common/config/SecurityConfig.java
@@ -1,9 +1,6 @@
 package com.tourgether.tourgether.common.config;
 
 import com.tourgether.tourgether.auth.filter.JwtAuthenticationFilter;
-import com.tourgether.tourgether.auth.oauth.handler.OAuth2AuthenticationFailureHandler;
-import com.tourgether.tourgether.auth.oauth.handler.OAuth2AuthenticationSuccessHandler;
-import com.tourgether.tourgether.auth.oauth.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,9 +18,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
   private final JwtAuthenticationFilter jwtAuthenticationFilter;
-  private final CustomOAuth2UserService customOAuth2UserService;
-  private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
-  private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -43,19 +37,11 @@ public class SecurityConfig {
                         "/h2-console/**",
                         "/swagger-ui/**",
                         "/v3/api-docs/**",
-                        "/swagger-ui.html")
+                        "/swagger-ui.html",
+                            "/api/v1/oauth2/**")
                     .permitAll().anyRequest().authenticated()
         )
 
-        .oauth2Login((oauth2) ->
-            oauth2
-                .userInfoEndpoint((userInfo) ->
-                    userInfo
-                        .userService(customOAuth2UserService)
-                )
-                .successHandler(oAuth2AuthenticationSuccessHandler)
-                .failureHandler(oAuth2AuthenticationFailureHandler)
-        )
         .headers((headers) ->
             headers
                 .frameOptions(FrameOptionsConfig::disable)
