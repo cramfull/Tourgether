9ca21d5966d0713df55d46cb634e035bae37ca3c
cramfull <130737411+cramfull@users.noreply.github.com>
2025-05-03 21:43:26 +0900

    fix: cors settings (#89)
    * feat: OAuthUserInfo, auth 진행중
    
    * chore: remove .idea from tracking
    
    * pulled develop
    
    * WIP: local application.yml
    
    * feat: CustomOAuth2User, auth 진행중
    
    * feat: OAuth2Authentication Handler, auth 진행중
    
    * feat: JwtAuthenticationFilter 진행중
    
    * fix: develop 충돌 해결
    
    * feat: reissue 구현
    
    * fix: MemberService 수정, GlobalExceptionHandler 수정
    
    * fix: AuthService 컴포넌트 수정, .yml jwt-secretKey 환경 변수로 사용
    
    * fix: RefreshTokenRequest validation 추가, JwtUtil @Value 수정
    
    * feat: AccessToken, RefreshToken 발급
    
    * fix: google 로직 수정
    
    * fix: redis docker conf
    
    * feat: Token Validate api
    
    * feat: 배포 front server CORS 적용
    
    * fix: prod yml jpa ddl options -> validate
    
    * fix: cors settings
    
    * fix: cors 설정

--- src/main/java/com/tourgether/tourgether/common/config/SecurityConfig.java
+++ src/main/java/com/tourgether/tourgether/common/config/SecurityConfig.java
@@ -15,6 +15,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -58,25 +60,28 @@ public class SecurityConfig {
         return http.build();
     }
 
+
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        //허용할 url 설정
-        configuration.addAllowedOrigin("http://localhost:5173");
-        configuration.addAllowedOrigin("https://www.tourgether.site");
-        configuration.addAllowedOrigin("https://www.tourgether.shop");
 
-        //허용할 헤더 설정
-        //허용할 http method
-        configuration.addAllowedMethod(CorsConfiguration.ALL);
-        configuration.addAllowedHeader(CorsConfiguration.ALL);
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://www.tourgether.site",
+                "https://tourgether.site",
+                "https://www.tourgether.shop",
+                "https://tourgether.shop",
+                "http://localhost:5173"
+        ));
 
-        //사용자 자격 증명이 지원되는지 여부
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
 }
