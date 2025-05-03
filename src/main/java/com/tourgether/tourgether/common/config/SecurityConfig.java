4f8f3851e1813e6787f8de9fa1ae0a3699dadc74
cramfull <130737411+cramfull@users.noreply.github.com>
2025-05-03 22:57:24 +0900

    fix: CORS 설정 헤더 인증 로직 없이 통과 (#91)
    * feat: OAuthUserInfo, auth 진행중
    
    * chore: remove .idea from tracking
    
    * pulled develop
    
    * WIP: local application.yml
    
    * feat: CustomOAuth2User, auth 진행중
    
    * feat: OAuth2Authentication Handler, auth 진행중
    
    * feat: JwtAuthenticationFilter 진행중
    
    * fix: develop 충돌 해결
    
    * feat: reissue 구현
    
    * fix: MemberService 수정, GlobalExceptionHandler 수정
    
    * fix: AuthService 컴포넌트 수정, .yml jwt-secretKey 환경 변수로 사용
    
    * fix: RefreshTokenRequest validation 추가, JwtUtil @Value 수정
    
    * feat: AccessToken, RefreshToken 발급
    
    * fix: google 로직 수정
    
    * fix: redis docker conf
    
    * feat: Token Validate api
    
    * feat: 배포 front server CORS 적용
    
    * fix: prod yml jpa ddl options -> validate
    
    * fix: cors settings
    
    * fix: CORS 설정 헤더 인증 로직 없이 통과

--- src/main/java/com/tourgether/tourgether/common/config/SecurityConfig.java
+++ src/main/java/com/tourgether/tourgether/common/config/SecurityConfig.java
@@ -65,6 +65,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
+
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "https://www.tourgether.site",
